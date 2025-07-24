4b03cc4b787c76f99ece27d3b9af6ce703a7ecaa
peter-lyr <llydrp.ldp@gmail.com>
2025-07-24 15:38:20 +0800

    <leader>di delete cur file

--- plugin/leader_d.lua
+++ plugin/leader_d.lua
@@ -1,7 +1,8 @@
-
 local F = require 'f'
 
 require 'which-key'.register {
   ['<leader>d'] = { name = 'd', },
   ['<leader>d<leader>'] = { name = 'd.more', },
+
+  ['<leader>di'] = { function() F.delete_files() end, 'delete cur file', mode = { 'n', 'v', }, silent = true, },
 }
